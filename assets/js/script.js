93d8dccbf84112091be46cc2033b4f3f97823524
wendelsagrilo <wendelsagrilo@gmail.com>
2018-03-02 15:06:23 -0300

    Tela de Seleçao de Personages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
+document.getElementsByTagName("body")[0].style.cursor = "url('../img/cursorBox.png'), auto";
+
 $(function(){
+    //Animação tela Escura
     $("#darkScreen").animate({
         height: "100vh",
     },1000);
+    
+   positionElement();
+
+   
+});
+
+    //Posicionamento dos textos e do botão
+function positionElement(){
 
     var positionX = $("#baloon").offset();
     var topImage = positionX.top + 90;
     var leftImage = positionX.left +150;
 
     
+    var positionY = $("#baloon").offset();
+    var top2 = positionY.top + 80;
+
+
+    var positionZ = $("#baloon").offset();
+    var top3 = positionZ.top + 160;
+    
     
     setInterval(function(){
+    
         $("#btnStart").animate({
             opacity: 1,
         },300);
@@ -18,26 +37,20 @@ $(function(){
             top: topImage,
             margin: '10px', 
         });
-    
-        var positionY = $("#baloon").offset();
-        var top2 = positionY.top + 80;
-        
         $("#textGame2").css({
             top: top2,
             margin: '50px',
         });
     
-        var positionZ = $("#baloon").offset();
-        var top3 = positionZ.top + 160;
-        
         $("#btnStart").css({
             top: top3,
             margin: '50px',
         });
        
     },1001);
-});
+}
 
+//Start Game
 $("#btnStart").on("click", function(){
     $("#darkScreen").animate({
         height: "0",
@@ -50,9 +63,34 @@ $("#btnStart").on("click", function(){
     $("#baloon").css({
         width: 0,
         height: 0,
-        zIndex: "-300",
-            
-        
-    })
+        zIndex: "-300",     
+    });
 
+    setInterval(function(){
+        $("#darkScreen, #blackBar,.text-game, #btnStart,#baloon").remove();
+    },1001);
+    
+    selectHero();
 });
+
+function selectHero(){
+    $("#player1, #player2, #vs").animate({
+        opacity: 1,
+    },600);
+
+    var character = document.getElementsByClassName('hero');
+    console.log(character);
+
+    for(var i = 0; i< character.length; i++){
+        character[i].addEventListener("mouseover", function(){
+            console.log(this.id);
+            this.style.borderBottom = "3px solid black";
+
+        });
+
+        character[i].addEventListener("mouseout", function(){
+            this.style.borderBottom = "none";
+
+        });
+    }
+}
